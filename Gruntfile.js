e4a14c2f942d6bcd3ecd7a87fa475625b8389413
dungvt <dungvt.dev@gmail.com>
2017-03-27 01:13:36 +0700

    add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-include-source');
+
+    grunt.initConfig({
+        includeSource:{
+            options:{
+                basePath: "parallx",
+                baseUrl: "parallx",
+            },
+            myTarget: {
+                files:{
+                    './index.html': './index.tpl.html'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', [
+        'includeSource',
+    ])
+}
---
+++ parallx/__testbed/d3_testbed.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict'
+
+    // Update…
+    var p = d3.select("body")
+        .selectAll("p")
+        .data([4, 8, 15, 16, 23, 42])
+        .text(function (d) { return d; });
+
+    // Enter…
+    p.enter().append("p")
+        .text(function (d) { return d; });
+
+    // Exit…
+    p.exit().remove();
+})();
